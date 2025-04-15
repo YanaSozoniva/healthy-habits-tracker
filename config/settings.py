b7b4801d34d496551dcbf70415956335df8566cc
Yana Sozonova <ynakochneva@mail.ru>
2025-04-15 16:50:43 +0500

    настройка CI/CD

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -163,3 +164,11 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db/sqlite3",
+        }
+    }
